589d75e3a57c0e0f51ec6e03cc0a433dadc1e743
Valentin <prelovskyvalentine@gmail.com>
2017-03-06 15:21:48 +0800

    Minimize and optimize

--- Unit1.pas
+++ Unit1.pas
@@ -15,8 +15,6 @@ type
     Timer1: TTimer;
     Memo2: TMemo;
     Label1: TLabel;
-    Button5: TButton;
-    Button6: TButton;
     Label2: TLabel;
     Label3: TLabel;
     Timer2: TTimer;
@@ -36,7 +34,6 @@ type
     procedure Button5Click(Sender: TObject);
     procedure Button6Click(Sender: TObject);
     procedure IdTCPClient1Disconnected(Sender: TObject);
-    procedure Button4Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Timer2Timer(Sender: TObject);
     procedure FormPaint(Sender: TObject);
@@ -66,8 +63,8 @@ implementation
 
 procedure switchControls(state:boolean);
 begin
-  Form1.Button5.Enabled:=state;
-  Form1.Button6.Enabled:=state;
+  //Form1.Button5.Enabled:=state;
+  //Form1.Button6.Enabled:=state;
 end;
 procedure disableControls();begin switchControls(false);end;
 procedure enableControls();begin switchControls(true);end;
@@ -79,7 +76,6 @@ end;
 procedure log(strin:string);
 begin
   WriteLn(logFile,strin);
-  WriteLn(logFile);
   Flush(logFile);
   Form1.Memo1.Lines.Add(strin);
 end;
@@ -101,37 +97,37 @@ begin
   
 end;
 procedure TForm1.Timer1Timer(Sender: TObject);
-var b:boolean;
+var b:boolean;locName:string;
 begin
-    //up();
     // Label3.Caption:=IntToStr(usb_counter);
-    Label2.Caption:=(IdTCPClient1.LocalName);
-    Label1.Caption :=  'client local name: ' + IdTCPClient1.LocalName;
+    locName:=    IdTCPClient1.LocalName;
+    Label2.Caption:=locName;
+    Label1.Caption :=  'client local name: ' + locName;
     b := idTCPClient1.Connected;
 
     try
       IdTCPClient1.CheckForDisconnect(false);
-      IdTCPClient1.Socket.Binding.UpdateBindingPeer;
+      //IdTCPClient1.Socket.Binding.UpdateBindingPeer;
       IdTCPClient1.SendCmd('alive');
       b := idTCPClient1.Connected;
 
       Form1.Caption:=BoolToStr(b);
     except
-      Label2.Caption:='ex 2';
+      //Label2.Caption:='ex 2';
     end;
 
     if (b = false) then
     begin
       try
-        Form1.Label6.Caption:='Не подлючен';
-
-        IdTCPClient1.Connect();
-
+        Form1.Label6.Caption:='Не подключен';
+        try
+          IdTCPClient1.Connect();
+        except end;
         IdTCPClient1.SendCmd('test');
         enableControls();
        except
         //Form1.Caption := 'cannot connect';
-        Form1.Label6.Caption:= 'cannot connect';
+        Form1.Label6.Caption:= 'не могу соединиться';
         disableControls();
         IdTCPClient1.Disconnect();
       end;
@@ -148,13 +144,13 @@ begin
     if (e = 'read_pc_name')then
     begin
       IdTCPClient1.WriteLn('read_pc_name');
-      log('read pc = client:' + IdTCPClient1.ReadLn);
+      log('Чтение имени клиента: ' + IdTCPClient1.ReadLn());
     end; // read pc name
 
     if (e = 'get_ip') then
     begin
       IdTCPClient1.WriteLn('get_ip');
-      log(IdTCPClient1.ReadLn);
+      log('Адрес = ' + IdTCPClient1.ReadLn());
     end; // get_ip
 
   end;// if (e <> '') then
@@ -164,27 +160,21 @@ procedure TForm1.Button5Click(Sender: TObject);
 var s:string;
 begin
   IdTCPClient1.WriteLn('read_pc_name');
-  s:= IdTCPClient1.ReadLn; // try to read
-  log('read pc = client:' + s);
+  s:= IdTCPClient1.ReadLn(); // try to read
+  log('Иет read pc = client:' + s);
 end;
 
 procedure TForm1.Button6Click(Sender: TObject);
 begin
   IdTCPClient1.WriteLn('get_ip');
-  Memo1.Lines.Add(IdTCPClient1.ReadLn);
+  Memo1.Lines.Add('' + IdTCPClient1.ReadLn());
 end;
 
 procedure TForm1.IdTCPClient1Disconnected(Sender: TObject);
 begin
   Timer1.Enabled:=false;
-  // ShowMessage('disconnected event');
 end;
 
-procedure TForm1.Button4Click(Sender: TObject);
-begin
-  IdTCPClient1.SendCmd('mouse_inject');
-  log('mouse_inject');
-end;
 function getFileName():string;begin
 Result:=('log ' + DateToStr(Now())+'-'+IntToStr(HourOf(Now))+'_'+IntToStr(MinuteOf(Now)) +'.txt');
 end;
@@ -218,22 +208,19 @@ end;
 procedure TForm1.Timer2Timer(Sender: TObject);
 begin
   //Form1.Visible:=false;
-  //up();
   Label3.Caption:=IntToStr(usb_counter);
-  IdTCPClient1.CheckForDisconnect(false);
-  if (IdTCPClient1.Connected = true) then IdTCPClient1.SendCmd('alive');
   
   if (lc <> usb_counter) then
   begin
     if (usb_counter > lc) then
       begin
         if (IdTCPClient1.Connected = true) then IdTCPClient1.SendCmd('mouse_inject');
-        log('inject');
+        log('Мышь извлечена');
       end;
     if (usb_counter < lc) then
       begin
         if (IdTCPClient1.Connected = true) then IdTCPClient1.SendCmd('mouse_eject');
-        log('ejct');
+        log('Мышь присоединена');
       end;
     lc := usb_counter; // equallly
   end;
@@ -261,11 +248,17 @@ begin
 try
     IdTCPClient1.Host:=srvHost;
     IdTCPClient1.SendCmd('alive');
-    Form1.Label6.Caption:='alive';
+    Form1.Label6.Caption:='Подключен';
   except
-    Form1.Label6.Caption:='alive NOT';
+    Form1.Label6.Caption:='Отключен';
+    Form1.Timer1.Enabled:=false;
     try
-     if (IdTCPClient1.Connected = false)then IdTCPClient1.Connect();
+     if (IdTCPClient1.Connected = false)then
+     begin
+      IdTCPClient1.Connect();
+      IdTCPClient1.SendCmd('alive');
+      Form1.Timer1.Enabled:=true;
+     end;
     except
     end;
   end;
